ed244af5e391c97baaa435e91d0229b521c5c07f
HACKERMASHINE\Sven <svenchurus@gmail.com>
2024-06-14 02:30:07 +0200

    Filters added to bucher.html and autoren.html

--- app/routes.py
+++ app/routes.py
@@ -1,5 +1,5 @@
 from flask import Blueprint, render_template, request
-from .models import db, Autor, Buch, Genre
+from .models import db, Autor, Buch, Genre, Subgenre
 
 main_routes = Blueprint('main_routes', __name__)
 
@@ -11,20 +11,37 @@ def home():
 #Autoren 
 @main_routes.route('/autoren')
 def autoren():
-    autoren = Autor.query.all()
+    nationalitat = request.args.get('nationalitat')
+    if nationalitat:
+        autoren = Autor.query.filter_by(nationalitat=nationalitat).all()
+    else:
+        autoren = Autor.query.all()
     return render_template('autoren.html', autoren=autoren)
 
 #Bucher
 @main_routes.route('/bucher')
 def bucher():
-    bucher = Buch.query.all()
-    return render_template('bucher.html', bucher=bucher)
+    subgenre = request.args.get('subgenre')
+    if subgenre:
+        bucher = Buch.query.join(Subgenre).filter(Subgenre.subgenre==subgenre).all()
+    else:
+        bucher = Buch.query.all()
+    
+    order = request.args.get('order', default=None)
+    if order == 'desc':
+        bucher = Buch.query.order_by(Buch.preis.desc()).all()
+    elif order == 'asc':
+        bucher = Buch.query.order_by(Buch.preis.asc()).all()
+    return render_template('bucher.html', bucher=bucher, order=order)
 
 #Genre
 @main_routes.route('/genres')
 def genre_subgenre():
     genres = Genre.query.all()
     return render_template('genre_subgenre.html', genres=genres)
+
+
+#
 
 
 
